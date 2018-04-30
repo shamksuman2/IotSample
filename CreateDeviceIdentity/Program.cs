a4a637742345f77b73a66ae432e70582b2a20452
shamksuman@live.com <shamksuman@live.com>
2018-04-30 16:48:18 +0530

    Initial commit

---
+++ CreateDeviceIdentity/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        private static RegistryManager registryManager;
+        private static string connnectionString = "HostName=iothubspike.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Um0WZ8l+W4SBv//fjMUEkD16MMQ70ens38geihI5hjs=";
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connnectionString);
+            AddDevice().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDevice()
+        {
+            string deviceId = "devicespike";
+            Device device;
+
+            try
+            {
+                device =await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
---
+++ SimulatedDevice/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        private static DeviceClient deviceClient;
+        private static string iotHubUri = "iothubspike.azure-devices.net";
+        private static string deviceKey = "ya5SviUeLQ4k4XJjekMTM0yka4lT4hXQTJ7msvCZ0kI=";
+        private static string deviceId = "devicespike";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey), TransportType.Mqtt);
+            deviceClient.ProductInfo = "HappyPath-simulated-device";
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            int messageId = 1;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemparature = minHumidity + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                var telematryDataPoint = new
+                {
+                    messageId = messageId++,
+                    deviceId = deviceId,
+                    temparature = currentTemparature,
+                    humidity = currentHumidity
+                };
+
+                var messageString = JsonConvert.SerializeObject(telematryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                message.Properties.Add("temparatureAlert", (currentTemparature > 30) ? "true": "false");
+                await deviceClient.SendEventAsync(message);
+
+                Console.WriteLine($"{DateTime.Now} > Sending message {messageString}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
